e768076e3151948b44c042a584b6999ac22e4938
thesuperpig56 <97651743+thesuperpig56@users.noreply.github.com>
2022-11-11 20:34:39 -0800

    Moved from another repository. [Initial commit]

---
+++ copyrights.hx
@@ -0,0 +1,26 @@
+package synthex;
+
+class Copyrights
+{
+	// 👹 - Midnight (10/23/2022 at 1:31 PM)
+	// 🤦‍♀️ - Midnight (10/23/2022 at 1:39 PM)
+	// 💜 - Midnight (10/23/2022 at 1:46 PM)
+	// 🤓 - Midnight (10/23/2022 at 1:47 PM)
+	// 😡 - Midnight (10/23/2022 at 1:49 PM)
+	// 👀 - Midnight (10/23/2022 at 1:50 PM)
+	// 👺 - Midnight (10/23/2022 at 1:50 PM)
+	// 😈 - Midnight (10/23/2022 at 1:50 PM)
+	// 🥹 - Midnight (10/23/2022 at 1:51 PM)
+	// 😴 - Midnight (10/23/2022 at 1:52 PM)
+	// Sucks to suck - Raccoon (10/23/2022 at 1:52 PM)
+	// 💀 - Midnight (10/23/2022 at 1:53 PM)
+	// anyone wanna join vc? - Raccoon (10/23/2022 at 1:56 PM)
+	// 🐼 - Midnight (10/23/2022 at 1:57 PM)
+	// 🥺 - Midnight (10/23/2022 at 2:01 PM)
+	// 😍 - Midnight (10/23/2022 at 2:01 PM)
+	// 😳 - Midnight (10/23/2022 at 2:02 PM)
+	// 🥵 - Midnight (10/23/2022 at 2:02 PM)
+	// ✨ - Midnight (10/23/2022 at 2:02 PM)
+    // ⭐️ - Midnight (10/23/2022 at 2:02 PM)
+	// Bye Bye - Midnight (10/23/2022 at 4:29 PM)
+}
